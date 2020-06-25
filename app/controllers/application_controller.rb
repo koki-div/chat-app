173ca17d3d4377331c43a068514fb79019adc846
koki-div <64241996+koki-div@users.noreply.github.com>
2020-06-25 10:55:53 +0900

    （コンフリクト解消後 - ユーザー管理機能を一旦進めた後に、もう一度フロント実装に戻ってきた）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  # deviseで利用可能なパラメーターを設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])   # ストロングパラメーターを追加 -> DBにnameカラムの保存を可能にする
+  end
 end
